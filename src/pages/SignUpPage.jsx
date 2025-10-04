90c8160719405000538094186a06e1e286f5414d
Ariel Diaz <mtgkm1@gmail.com>
2025-10-04 15:33:53 -0300

    Se crea y agrega componete TileForm a los diferente comp

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,9 +1,42 @@
-const SignUpPage = () =>{
-    return (
-        <div>
-          <h1>Sign Up</h1>            
+import TitleForm from "../components/titleForm";
+import FormField from "../components/FormField";
+import { KeyRound, Mail, PhoneCallIcon, User2Icon } from "lucide-react";
+import { useState } from "react";
+
+const SignUpPage = () => {
+  const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [cellphone, setCellphone] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Name", name);
+    console.log("Apellido", lastName);
+    console.log("celu", cellphone);
+    console.log("Email", email);
+  }
+
+  return (
+    <div>
+      <TitleForm title={"Registrarse"} />
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10 ">
+          <form onSubmit={handleSubmit} className="space-y-10">
+            <FormField fieldName="nombre" FieldIcon={User2Icon} fieldValue={name} onChange={setName} />
+            <FormField fieldName="Apellido" FieldIcon={User2Icon} fieldValue={lastName} onChange={setLastName} />
+            <FormField fieldName="teléfono" FieldIcon={PhoneCallIcon} fieldValue={cellphone} onChange={setCellphone} />
+            <FormField fieldName="email" FieldIcon={Mail} fieldValue={email} onChange={setEmail} />
+            <button className="w-full flex justify-center border rounded-md 
+            border-transparent bg-emerald-600 py-2 px-4 text-sm font-medium text-white hover:bg-emerald-700
+            focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2"
+              type="submit">Enviar</button>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage
