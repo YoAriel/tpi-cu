8922257679a03f2d6abe106bce002423fafc8d01
Ariel Diaz <mtgkm1@gmail.com>
2025-10-09 16:35:59 -0300

    feat:se creo la ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import HomePage from './pages/HomePage';
-import SignUpPage from './pages/SignUpPage';
-import LoginPage from './pages/LoginPage';
-import Navbar from './components/Navbar';
-import AdminPage from './pages/AdminPage';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import Navbar from "./components/Navbar";
+import AdminPage from "./pages/AdminPage";
+import CategoryPage from "./pages/CategoryPage";
 
 const App = () => {
   const user = { role: "admin" };
@@ -13,12 +19,24 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={!userLogin ? <SignUpPage /> : <Navigate to="/" />} />
-        <Route path="/login" element={!userLogin ? <LoginPage /> : <Navigate to="/" />} />
-        <Route path="/secret-dashboard" element={user.role === "admin" ? <AdminPage /> : <Navigate to="/login" />} />
+        <Route
+          path="/signup"
+          element={!userLogin ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!userLogin ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/secret-dashboard"
+          element={
+            user.role === "admin" ? <AdminPage /> : <Navigate to="/login" />
+          }
+        />
+        {/* <Route path="/category/:category" element={<CategoryPage />} /> */}
       </Routes>
     </Router>
   );
-}
+};
 
-export default App
+export default App;
