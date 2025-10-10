27262ba82383f43c850859bf1b92eb653e2a276d
Ariel Diaz <mtgkm1@gmail.com>
2025-10-10 15:15:19 -0300

    se creo el comp ProducCard

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import ProductCard from "../components/ProductCard";
 
 const CategoryPage = () => {
   const { category } = useParams();
